768198a4c2cb8343f9100e30dc9d49adc4a090e5
Michai329 <mpryce30@gmail.com>
2020-11-11 10:40:27 -0500

    Included basic input & output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,21 @@
+/*
+ * Author: Michai Pryce
+ * Date: November 11, 2020
+ */
+
 package ca.durhamcollege;
+
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+    public static void main(String[] args)
+    {
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You Entered: %S",name);
     }
 }
